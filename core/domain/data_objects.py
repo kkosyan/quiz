e86e264cd6bb324a864ee156565b2264b2083da7
kkosyan <lizhenina.k@gmail.com>
2023-02-26 22:36:37 +0300

    add classmethods

--- core/domain/data_objects.py
+++ core/domain/data_objects.py
@@ -30,6 +30,18 @@ class QuestionDto:
     text: Optional[str] = None
     attachment: Optional[bytes] = None
 
+    @classmethod
+    def build(cls, question_id: ObjectId, question_type: QuestionType, answer_id: ObjectId, question_price: int,
+              text: Optional[str] = None, attachment: Optional[bytes] = None) -> 'QuestionDto':
+        return QuestionDto(
+            question_id=question_id,
+            question_type=question_type,
+            answer_id=answer_id,
+            question_price=question_price,
+            text=text,
+            attachment=attachment,
+        )
+
 
 @dataclass
 class AnswerDto:
@@ -40,27 +52,62 @@ class AnswerDto:
     text: Optional[str] = None
     attachment: Optional[bytes] = None
 
+    @classmethod
+    def build(cls, question_id: ObjectId, answer_type: AnswerType, answer_id: ObjectId, answer_price: int,
+              text: Optional[str] = None, attachment: Optional[bytes] = None) -> 'AnswerDto':
+        return AnswerDto(
+            question_id=question_id,
+            answer_type=answer_type,
+            answer_id=answer_id,
+            answer_price=answer_price,
+            text=text,
+            attachment=attachment,
+        )
+
 
 @dataclass
-class Task:
-    id: str
-    question: QuestionDto
-    answer: AnswerDto
+class TaskDto:
+    task_id: ObjectId
+    task_name: str
+    task_questions: list[ObjectId]
+
+    @classmethod
+    def build(cls, task_id: ObjectId, task_name: str, task_questions: list[ObjectId]) -> 'TaskDto':
+        return TaskDto(
+            task_id=task_id,
+            task_name=task_name,
+            task_questions=task_questions,
+        )
 
 
 @dataclass
-class Round:
-    id: str
+class RoundDto:
+    round_id: ObjectId
     name: str
-    questions: list[Task]
+    round_tasks: list[ObjectId]
+
+    @classmethod
+    def build(cls, round_id: ObjectId, name: str, round_tasks: list[ObjectId]) -> 'RoundDto':
+        return RoundDto(
+            round_id=round_id,
+            name=name,
+            round_tasks=round_tasks,
+        )
 
 
 @dataclass
-class Game:
-    id: str
+class GameDto:
+    game_id: ObjectId
     name: str
-    rounds: list[Round]
-    type: str
+    game_rounds: list[ObjectId]
+
+    @classmethod
+    def build(cls, game_id: ObjectId, name: str, game_rounds: list[ObjectId]) -> 'GameDto':
+        return GameDto(
+            game_id=game_id,
+            name=name,
+            game_rounds=game_rounds,
+        )
 
 
 Balance = NewType('Balance', int)
