549f903595ac8f612f65287ddc3e298fe1f29965
kkosyan <lizhenina.k@gmail.com>
2023-02-26 22:37:06 +0300

    summarize adapters

---
+++ adapters/fauna_database_saver.py
@@ -0,0 +1,23 @@
+from dataclasses import asdict
+from typing import Union, Type
+
+from faunadb import query
+from faunadb.client import FaunaClient
+
+from core.domain.data_objects import QuestionDto, AnswerDto, TaskDto, RoundDto, GameDto
+from core.services.database_saver import DatabaseSaver
+
+
+class FaunaDatabaseSaver(DatabaseSaver):
+    def __init__(self, client: FaunaClient, collection: str):
+        self.client = client
+        self.collection = collection
+
+    def save(self, dto: Union[Type[QuestionDto], Type[AnswerDto], Type[TaskDto], Type[RoundDto], Type[GameDto]]):
+        self.client.query(
+            query.create(
+                query.collection(self.collection), {
+                    'data': {asdict(dto)}
+                }
+            )
+        )
