b8258dcb3146920bd9456eb6fb6dac2492591e13
kkosyan <lizhenina.k@gmail.com>
2023-02-28 20:56:19 +0300

    change adapter saver

--- adapters/fauna_database_saver.py
+++ adapters/fauna_database_saver.py
@@ -9,14 +9,13 @@ from core.services.database_saver import DatabaseSaver
 
 
 class FaunaDatabaseSaver(DatabaseSaver):
-    def __init__(self, client: FaunaClient, collection: str):
+    def __init__(self, client: FaunaClient):
         self.client = client
-        self.collection = collection
 
-    def save(self, dto: Union[QuestionDto, AnswerDto, TaskDto, RoundDto, GameDto]):
+    def save(self, dst: str, dto: Union[QuestionDto, AnswerDto, TaskDto, RoundDto, GameDto]):
         self.client.query(
             query.create(
-                query.collection(self.collection), {
+                query.collection(dst), {
                     'data': asdict(dto)
                 }
             )
