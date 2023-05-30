d98b6041fdacdcbb0992e4d4448da72a8803edf8
kkosyan <lizhenina.k@gmail.com>
2023-05-31 02:29:00 +0300

    add app entrypoint

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,15 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
+from config import config
+
 db = SQLAlchemy()
 
 
 def create_app():
     app = Flask(__name__)
+    app.config.from_object(config['production'])
+    config['production'].init_app(app)
     db.init_app(app)
 
     return app
