841ebc6410856e09dd159aed571111aa8cbfb776
kkosyan <lizhenina.k@gmail.com>
2023-05-31 03:32:59 +0300

    change init

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,12 @@
 from flask import Flask
+from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
 
 from config import config
+from .main import main as main_blueprint
 
 db = SQLAlchemy()
+bootstrap = Bootstrap()
 
 
 def create_app():
@@ -11,5 +14,8 @@ def create_app():
     app.config.from_object(config['production'])
     config['production'].init_app(app)
     db.init_app(app)
+    bootstrap.init_app(app)
+
+    app.register_blueprint(main_blueprint)
 
     return app
