5f2c4c81c3ac52b09b6a7350b93a68a26ab5bd42
kkosyan <lizhenina.k@gmail.com>
2023-06-01 22:47:25 +0300

    finalize view

--- app/main/views.py
+++ app/main/views.py
@@ -1,15 +1,23 @@
-from flask import render_template
+from flask import render_template, redirect, url_for, session
 
 from . import main
 from .forms import CreateNewGameForm
+from .utils import UuidIdGenerator
+from .. import db
+from ..models import Game
+
+id_generator = UuidIdGenerator()
 
 
 @main.route('/', methods=['GET', 'POST'])
 def index():
-    new_game_name = None
     form = CreateNewGameForm()
     if form.validate_on_submit():
         new_game_name = form.game_name.data
+        new_game = Game(game_id=id_generator.generate(), game_name=new_game_name)
+        db.session.add(new_game)
+        db.session.commit()
+        session['new_game_name'] = form.game_name.data
         form.game_name.data = ''
-
-    return render_template('create_game.html', form=form)
+        return redirect(url_for('main.index'))
+    return render_template('create_game.html', form=form, new_game_name=session.get('new_game_name'))
