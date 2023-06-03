070ef18041a8a694c62c0e75c6f76056c9d52b5d
kkosyan <lizhenina.k@gmail.com>
2023-06-03 23:00:48 +0300

    add new forms

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,9 +1,54 @@
 from flask_wtf import FlaskForm
-from wtforms.validators import DataRequired
-from wtforms import StringField, TextAreaField, BooleanField, SelectField,\
-    SubmitField
+from wtforms import StringField, SubmitField, TextAreaField, FileField, URLField, BooleanField
+from wtforms.validators import DataRequired, Optional
 
 
 class CreateNewGameForm(FlaskForm):
     game_name = StringField('New game name', validators=[DataRequired()])
     submit = SubmitField('Submit')
+
+
+class CreateNewRound(FlaskForm):
+    round_name = StringField('Round name:', validators=[DataRequired()])
+    start_price = StringField('Start price:', validators=[DataRequired()])
+
+    submit_new_round = SubmitField('Submit new round')
+
+
+class CreateNewCategory(FlaskForm):
+    category_name = StringField('Category name:', validators=[DataRequired()])
+    question_1_text = TextAreaField('Text:', validators=[Optional()])
+    question_1_file = FileField('File:', validators=[Optional()])
+    question_1_url = URLField('URL:', validators=[Optional()])
+    question_2_text = TextAreaField('Text:', validators=[Optional()])
+    question_2_file = FileField('File:', validators=[Optional()])
+    question_2_url = URLField('URL:', validators=[Optional()])
+    question_3_text = TextAreaField('Text:', validators=[Optional()])
+    question_3_file = FileField('File:', validators=[Optional()])
+    question_3_url = URLField('URL:', validators=[Optional()])
+    question_4_text = TextAreaField('Text:', validators=[Optional()])
+    question_4_file = FileField('File:', validators=[Optional()])
+    question_4_url = URLField('URL:', validators=[Optional()])
+    question_5_text = TextAreaField('Text:', validators=[Optional()])
+    question_5_file = FileField('File:', validators=[Optional()])
+    question_5_url = URLField('URL:', validators=[Optional()])
+
+    answer_1_text = TextAreaField('Text:', validators=[Optional()])
+    answer_1_file = FileField('File:', validators=[Optional()])
+    answer_1_url = URLField('URL:', validators=[Optional()])
+    answer_2_text = TextAreaField('Text:', validators=[Optional()])
+    answer_2_file = FileField('File:', validators=[Optional()])
+    answer_2_url = URLField('URL:', validators=[Optional()])
+    answer_3_text = TextAreaField('Text:', validators=[Optional()])
+    answer_3_file = FileField('File:', validators=[Optional()])
+    answer_3_url = URLField('URL:', validators=[Optional()])
+    answer_4_text = TextAreaField('Text:', validators=[Optional()])
+    answer_4_file = FileField('File:', validators=[Optional()])
+    answer_4_url = URLField('URL:', validators=[Optional()])
+    answer_5_text = TextAreaField('Text:', validators=[Optional()])
+    answer_5_file = FileField('File:', validators=[Optional()])
+    answer_5_url = URLField('URL:', validators=[Optional()])
+
+    submit_category = SubmitField('Submit category')
+
+    create_new_category = BooleanField('Add category to the round?')
