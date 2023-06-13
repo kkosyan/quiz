70ad9b5561bf7250c95c16fe384ce3075a48fbd6
kkosyan <lizhenina.k@gmail.com>
2023-06-13 23:57:56 +0300

    ass saving

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,13 @@
 from flask import Flask
 from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
+from flask_uploads import configure_uploads, IMAGES, UploadSet
 
 from config import config
 
 db = SQLAlchemy()
 bootstrap = Bootstrap()
+images = UploadSet('images', IMAGES)
 
 
 def create_app():
@@ -14,6 +16,9 @@ def create_app():
     config['production'].init_app(app)
     db.init_app(app)
     bootstrap.init_app(app)
+
+
+    configure_uploads(app, images)
 
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
