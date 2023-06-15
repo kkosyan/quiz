4ac2d79a1922f4e34863a5d0b954318a96500ec8
kkosyan <lizhenina.k@gmail.com>
2023-06-15 23:12:45 +0300

    add index view

--- app/main/views.py
+++ app/main/views.py
@@ -12,16 +12,8 @@ id_generator = UuidIdGenerator()
 
 @main.route('/', methods=['GET', 'POST'])
 def index():
-    form = CreateNewGameForm()
-    if form.validate_on_submit():
-        new_game_name = form.game_name.data
-        new_game = Game(game_id=id_generator.generate(), game_name=new_game_name)
-        db.session.add(new_game)
-        db.session.commit()
-        session['new_game_name'] = form.game_name.data
-        form.game_name.data = ''
-        return redirect(url_for('main.index'))
-    return render_template('create_game.html', form=form, new_game_name=session.get('new_game_name'))
+    games = Game.query.all()
+    return render_template('index.html', games=games)
 
 
 @main.route('/creator_mode', methods=['GET', 'POST'])
